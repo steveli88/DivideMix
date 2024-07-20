d277d452db3d6d3cb11857f2f43415e8f7e26b11
steveli88 <stevel@kgsresearch.com>
2024-07-20 19:03:34 -0400

    reorder and add comments

--- Train_cifar.py
+++ Train_cifar.py
@@ -227,9 +227,10 @@ def linear_rampup(current, warm_up, rampup_length=16):
 
 class SemiLoss(object):
     def __call__(self, outputs_x, targets_x, outputs_u, targets_u, epoch, warm_up):
-        probs_u = torch.softmax(outputs_u, dim=1)
-
+        # labeled
         Lx = -torch.mean(torch.sum(F.log_softmax(outputs_x, dim=1) * targets_x, dim=1))
+        # unlabeled
+        probs_u = torch.softmax(outputs_u, dim=1)
         Lu = torch.mean((probs_u - targets_u) ** 2)
 
         return Lx, Lu, linear_rampup(epoch, warm_up)
